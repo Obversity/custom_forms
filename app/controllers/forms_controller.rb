606a45a2ecfc02350700d33c054ac3c551122e63
Jayme <cronhound@gmail.com>
2015-09-25 17:57:22 +0930

    Added validations, and a made a way for it to display the errors. Maybe make it neater by turning it into methods?

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -9,9 +9,30 @@ class FormsController < ApplicationController
 #     end
 
   def submit
-    binding.pry
-    @submission = Submission.create(data: params[:submission].to_json, form: Form.find(params[:submission][:form_id]))
-    redirect_to "/forms/read/#{@submission.id}"
+    data = params[:submission]
+    form = Form.find(params[:submission][:form_id])
+    errors = []
+    form.fields.each do |field|
+      field.field_validations.each do |validation|
+        validation_type = FieldValidation::VALIDATION_TYPES[validation.validation_id]
+        is_good = FieldValidation.send(validation_type, data[field.id.to_s])
+
+
+        errors << "#{field.label} does not validate with #{validation_type} correctly." unless is_good
+
+      end
+    end
+
+    if errors.length == 0
+      @submission = Submission.create(data: data.to_json, form: form)
+      redirect_to "/forms/read/#{@submission.id}"
+    else
+      session[:errors] = errors
+      session[:submission] = data
+
+      redirect_to "/forms/#{form.id}"
+    end
+
   end
 
   def read
--- app/models/field_validation.rb
+++ app/models/field_validation.rb
@@ -1,3 +1,22 @@
 class FieldValidation < ActiveRecord::Base
   belongs_to :field
+
+  VALIDATION_TYPES = {
+    1=>'required',
+    2=>'email',
+    3=>'regex',
+    4=>'datetime',
+    5=>'number',
+    6=>'length'
+  }
+
+
+  def self.required(value)
+    return !value.blank?
+  end
+
+  def self.email(value)
+    (value =~ /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i)
+  end
+
 end
