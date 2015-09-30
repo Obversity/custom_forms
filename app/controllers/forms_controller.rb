bd6f9d02f0d3c30e8b69294ba8f17b737470499d
Jayme <cronhound@gmail.com>
2015-09-30 13:49:46 +0930

    tidied code, added a tonne of comments

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -1,4 +1,5 @@
 class FormsController < ApplicationController
+
   def show
     @form = Form.find(params[:id])
   end
@@ -9,20 +10,33 @@ class FormsController < ApplicationController
 #     end
 
   def submit
-    data = params[:submission]
+
+    # get the form's fields
     form = Form.find(params[:submission][:form_id])
+
+    # get the data that was submitted
+    data = params[:submission]
+
+    # make sure that errors is defined as an array, otherwise things will break
     errors = []
+
+    # validate each field
     form.fields.each do |field|
       field.field_validations.each do |validation|
+
+        # check what kind of validation we're doing.
         validation_type = FieldValidation::VALIDATION_TYPES[validation.validation_id]
-        is_good = FieldValidation.send(validation_type, data[field.id.to_s])
 
+        # check whether the value validates
+        valid = FieldValidation.send(validation_type, data[field.id.to_s])
 
-        errors << "#{field.label} does not validate with #{validation_type} correctly." unless is_good
+        # push error messages into the errors array
+        errors << "#{field.label} does not validate with #{validation_type} correctly." unless valid
 
       end
     end
 
+    # if we have no errors, submit the form; otherwise reject the submission
     if errors.length == 0
       @submission = Submission.create(data: data.to_json, form: form)
       redirect_to "/forms/read/#{@submission.id}"
--- app/helpers/forms_helper.rb
+++ app/helpers/forms_helper.rb
@@ -1,18 +1,37 @@
 module FormsHelper
+
+  # method for displaying the errors after the form has been submitted and rejected
   def get_errors
-    if !session[:errors].nil?
+    # unless there are no errors
+    unless session[:errors].nil?
+      # pass the errors to the errors variable
       errors = session[:errors]
+
+      # clear the errors, so that they are only shown once.
       session[:errors] = nil
+
+      # pass the errors to the @errors variable
+      errors
+
     end
-    @errors = errors
   end
 
+  # method for displaying the form data after a form has been submitted and rejected
   def get_form_data
+    # make sure that data is always a hash
     data = {}
-    if !session[:submission].nil?
+
+    # unless there has been no submissions
+    unless session[:submission].nil?
+      # store the data in a temporary variable
       data = session[:submission]
+
+      # clear the session's submission, so that it is only shown once
       session[:submission] = nil
     end
+
+    #pass the data to the data variable
     @data = data
   end
+
 end
